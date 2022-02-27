0a5b929b80c74e7b11074994849b5e03d9335f04
{krism01} <{krism01@naver.com}>
2022-02-27 23:49:39 +0900

    nuxt.config.js - Add server port Add axios main.vue - import ProductList component Add ProductList.vue & asyncData setup

--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <p>메인</p>
+    <!-- <p>첫 번째 products: {{ products[0] }}</p> -->
+    <!-- <P>{{ products }}</P> -->
+    <ProductList></ProductList>
   </div>
 </template>
 
 <script>
-export default {}
+import ProductList from '../components/ProductList.vue'
+
+export default {
+  components: { ProductList },
+}
 </script>
 
 <style></style>
