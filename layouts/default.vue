53cd29133804451d598d39669cdd56272a6e403a
{krism01} <{krism01@naver.com}>
2022-02-27 18:09:18 +0900

    공통 페이지인 layouts/default.vue 에서 각각의 페이지로 접근하기 위한 router 설정하기

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template lang="">
   <div>
-    <h1>{{ $route.name }}페이지</h1>
+    <header>
+      <h1>{{ $route.name }}페이지</h1>
+      <NuxtLink to="/">home page</NuxtLink>
+      <NuxtLink to="/main">main page</NuxtLink>
+      <NuxtLink to="/product">product page</NuxtLink>
+    </header>
+    <Nuxt />
   </div>
 </template>
 <script>
 export default {
   created() {
     // eslint-disable-next-line no-console
-    console.log('this.$route', this.$route)
+    // console.log('this.$route', this.$route)
   },
 }
 </script>
