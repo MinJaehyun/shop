58c6b4937c99d801a52443dee26ceaa3d2951d64
{krism01} <{krism01@naver.com}>
2022-02-28 17:07:36 +0900

    상세페이지를 나타내기 위해 이 전 과정을 리펙토링하여 asyncData 에 인자를 넣고 api module 을 가져온다. 화면에 나타내고 css 적용.

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -1,20 +1,43 @@
 <template lang="">
-  <div>
-    {{ $route.params.id }}
-    {{ product }}
+  <div class="container">
+    <div class="main-panel">
+      <img :src="product.imageUrl" :alt="product.name" class="product-image" />
+    </div>
+    <div class="side-panel">
+      <p class="name">name: {{ product.name }}</p>
+      <p class="price">price: {{ product.price }}</p>
+      <button type="button">addToCart</button>
+    </div>
   </div>
 </template>
+
 <script>
-import axios from 'axios'
+import { fetchProductById } from '../../api/index'
 
 export default {
   async asyncData({ params }) {
-    const response = await axios.get(
-      `http://localhost:3000/products/${params.id}`
-    )
+    const response = await fetchProductById(params.id)
     const product = response.data
     return { product }
   },
 }
 </script>
-<style lang=""></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  widows: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
+</style>
