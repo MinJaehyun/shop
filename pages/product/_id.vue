eb7c82d637e35ec68ec6658bde97b1b01318c0c8
{krism01} <{krism01@naver.com}>
2022-02-28 16:30:43 +0900

    상세 페이지 설정

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -1,7 +1,20 @@
 <template lang="">
-  <div>{{ $route.params.id }}</div>
+  <div>
+    {{ $route.params.id }}
+    {{ product }}
+  </div>
 </template>
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const response = await axios.get(
+      `http://localhost:3000/products/${params.id}`
+    )
+    const product = response.data
+    return { product }
+  },
+}
 </script>
 <style lang=""></style>
