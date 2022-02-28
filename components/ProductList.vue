3485690c03c9ad2c4e540b855919ff02462ef20d
{krism01} <{krism01@naver.com}>
2022-02-28 15:42:52 +0900

    Add ProductList component

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,19 +1,9 @@
 <template>
-  <div>
-    {{ products }}
-  </div>
+  <div>ProductList 컴포넌트</div>
 </template>
 
 <script>
-import axios from 'axios'
-
-export default {
-  async asyncData() {
-    const response = await axios.get('http://localhost:3000/products')
-    const products = response.data
-    return { products }
-  },
-}
+export default {}
 </script>
 
 <style></style>
