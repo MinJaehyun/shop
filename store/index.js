83cc7048a8677f90733a811c806c77230571964c
{krism01} <{krism01@naver.com}>
2022-03-03 18:13:51 +0900

    actions 를 사용하여 비동기 함수를 동기적으로 처리

--- store/index.js
+++ store/index.js
@@ -1,3 +1,5 @@
+import { fetchCartItems } from "~/api"
+
 export const state = () => ({
   cartItems: [],
 })
@@ -13,4 +15,16 @@ export const mutations = {
     }
     state.cartItems.push(newCartItem)
   },
+  setCartItems(state, cartItems) {
+    state.cartItems = cartItems
+  }
+}
+
+export const actions = {
+  // commit 으로 mutations 가져오기 위한 함수
+  async FETCH_CART_ITEMS({ commit }) {
+    // 비동기 함수인 처리하기 위해 actions 에 설정한 함수
+    const { data } = await fetchCartItems()
+    commit('setCartItems', data)
+  }
 }
