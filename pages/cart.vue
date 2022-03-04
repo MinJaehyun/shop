92e207edbb63c3e0be95d30fa690684f4bb716d4
{krism01} <{krism01@naver.com}>
2022-03-04 13:47:49 +0900

    Change page based on component

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,30 +1,25 @@
 <template lang="">
   <div class="container">
     <h1 class="list-title">cart page</h1>
-    <div class="list-wrapper"></div>
-    <ul>
-      <li
-        v-for="item in $store.state.cartItems"
-        :key="item.id"
-        class="list-item"
-      >
-        <img :src="item.imageUrl" :alt="item.name" class="thumbnail" />
-        <div class="description">
-          <p>{{ item.name }}</p>
-          <p>{{ item.price }}</p>
-        </div>
-      </li>
-    </ul>
+    <!-- pages 의 내용을 component 기반으로 분리하여 사용 -->
+    <CartList></CartList>
+    <!--  -->
     <div class="extra-panel">
       <button type="button">구매하기</button>
     </div>
   </div>
 </template>
 <script>
+import CartList from '@/components/CartList.vue'
+
 export default {
-  async asyncData({ store }) {
-    await store.dispatch('FETCH_CART_ITEMS')
+  components: {
+    CartList,
   },
+  // component 로 분리했으므로 같이 이동시킨다
+  // async asyncData({ store }) {
+  //   await store.dispatch('FETCH_CART_ITEMS')
+  // },
 }
 </script>
 
@@ -35,19 +30,6 @@ export default {
 .list-title {
   font-weight: 700;
   font-size: 1.4rem;
-}
-.list-wrapper {
-  margin: 0.4rem 0;
-}
-.list-item {
-  display: flex;
-}
-.thumbnail {
-  width: 100px;
-  height: 100px;
-}
-.description {
-  padding: 2rem 1rem;
 }
 .extra-panel {
   text-align: right;
